f9ffa2faee8c441d1a72745ceba2918149acb8f6
Nathan Santos de Deus <nathandosantos@hotmail.com>
2026-03-10 15:06:02 +0000

    invoices controller done

--- Library.MVC/Models/InvoiceCreateViewModel.cs
+++ Library.MVC/Models/InvoiceCreateViewModel.cs
@@ -1,22 +1,28 @@
-﻿using Library.Domain;
+﻿using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace Library.MVC.Models
+namespace Library.MVC.ViewModels
 {
-    public class InvoiceCreateViewModel
+    public class CreateInvoiceViewModel
     {
+        [Required]
+        [Display(Name = "Invoice Date")]
+        [DataType(DataType.Date)]
+        public DateTime InvoiceDate { get; set; } = DateTime.Today;
+
+        [Required]
+        [Display(Name = "Customer")]
         public int CustomerId { get; set; }
 
-        public DateTime InvoiceDate { get; set; } = DateTime.UtcNow;
-
+        [Required]
+        [Display(Name = "Product")]
         public int ProductId { get; set; }
 
+        [Required]
+        [Range(1, 100000)]
         public int Quantity { get; set; }
 
-        public decimal UnitPrice { get; set; }
-
-        public List<SelectListItem>? Customers { get; set; }
-
-        public List<SelectListItem>? Products { get; set; }
+        public List<SelectListItem> Customers { get; set; } = new();
+        public List<SelectListItem> Products { get; set; } = new();
     }
 }
